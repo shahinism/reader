f235d4136174deab24ac32efcbaee4df142bc4b6
Shahin <ishahinism@gmail.com>
2016-07-04 22:59:38 +0430

    Dummy update to solve travis problem

--- db/schema.rb
+++ db/schema.rb
@@ -39,8 +39,8 @@ ActiveRecord::Schema.define(version: 20_160_704_135_445) do
     t.datetime 'last_sign_in_at'
     t.string   'current_sign_in_ip'
     t.string   'last_sign_in_ip'
-    t.datetime 'created_at',                          null: false
-    t.datetime 'updated_at',                          null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
     t.string   'username'
     t.string   'first_name'
     t.string   'last_name'
