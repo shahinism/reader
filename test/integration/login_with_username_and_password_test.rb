9444d16d802213b576d4aa09863e4ac98aff1572
Shahin <ishahinism@gmail.com>
2016-07-04 17:47:08 +0430

    Add custom user login test

---
+++ test/integration/login_with_username_and_password_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class LoginWithUsernameAndPasswordTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  def setup
+    @user = users(:one)
+    @password = 'password'
+  end
+
+  def login_test(user_data)
+    post new_user_session_path, user: user_data
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_equal 'Signed in successfully.', flash[:notice]
+  end
+
+  test "User should be able to login with username" do
+    login_test({ login: @user.username, password: @password })
+  end
+
+  test "User should be able to login with email" do
+    login_test({ login: @user.email, password: @password })
+  end
+
+end
