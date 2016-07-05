d6f5098a851d42656835b26ad2b17f5a864a832b
Shahin <ishahinism@gmail.com>
2016-07-05 13:59:21 +0430

    Use Rbenv to handle project dependencies

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class Feed < ApplicationRecord
   has_many :user_feeds
   has_many :users, through: :user_feeds
--- db/migrate/20160704135445_create_user_feeds.rb
+++ db/migrate/20160704135445_create_user_feeds.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class CreateUserFeeds < ActiveRecord::Migration[5.0]
   def change
     create_table :user_feeds do |t|
